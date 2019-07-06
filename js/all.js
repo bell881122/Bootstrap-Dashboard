80cfe877ca28fc12814629043957ccefcfe1b6a9
bell881122 <51412772+bell881122@users.noreply.github.com>
2019-07-07 07:45:53 +0800

    201907070730 upload

---
+++ js/all.js
@@ -0,0 +1,288 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+// 編輯Modal事件
+$(document).ready(function () {
+    $('#editModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var addTitle = button.data('title'); // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this);
+        modal.find('.modal-title').text('編輯 ' + addTitle + ' 訂單');
+        modal.find('.editItem').val(addTitle);
+        //  指定disabled標籤
+        // var valid = modal.button[data - title];
+        // if (valid !== "快速下單") {
+        //     modal.find('.editItem').attr('disabled', 'disabled');
+        // };
+        var valid = button.data('title');
+        if (valid !== "快速下單") {
+            modal.find('.editItem').attr('disabled', 'disabled');
+        } else {
+            modal.find('.editItem').removeAttr('disabled');
+        };
+    })
+    $('#removeModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var removeTitle = button.data('title'); // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        console.log('title', removeTitle);
+        var modal = $(this);
+        modal.find('.modal-title').text('確認刪除 ' + removeTitle + ' ？');
+    })
+});
+
+'use strict';
+
+window.chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
+};
+
+(function (global) {
+    var MONTHS = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+
+    var COLORS = [
+        '#4dc9f6',
+        '#f67019',
+        '#f53794',
+        '#537bc4',
+        '#acc236',
+        '#166a8f',
+        '#00a950',
+        '#58595b',
+        '#8549ba'
+    ];
+
+    var Samples = global.Samples || (global.Samples = {});
+    var Color = global.Color;
+
+    Samples.utils = {
+        // Adapted from http://indiegamr.com/generate-repeatable-random-numbers-in-js/
+        srand: function (seed) {
+            this._seed = seed;
+        },
+
+        rand: function (min, max) {
+            var seed = this._seed;
+            min = min === undefined ? 0 : min;
+            max = max === undefined ? 1 : max;
+            this._seed = (seed * 9301 + 49297) % 233280;
+            return min + (this._seed / 233280) * (max - min);
+        },
+
+        numbers: function (config) {
+            var cfg = config || {};
+            var min = cfg.min || 0;
+            var max = cfg.max || 1;
+            var from = cfg.from || [];
+            var count = cfg.count || 8;
+            var decimals = cfg.decimals || 8;
+            var continuity = cfg.continuity || 1;
+            var dfactor = Math.pow(10, decimals) || 0;
+            var data = [];
+            var i, value;
+
+            for (i = 0; i < count; ++i) {
+                value = (from[i] || 0) + this.rand(min, max);
+                if (this.rand() <= continuity) {
+                    data.push(Math.round(dfactor * value) / dfactor);
+                } else {
+                    data.push(null);
+                }
+            }
+
+            return data;
+        },
+
+        labels: function (config) {
+            var cfg = config || {};
+            var min = cfg.min || 0;
+            var max = cfg.max || 100;
+            var count = cfg.count || 8;
+            var step = (max - min) / count;
+            var decimals = cfg.decimals || 8;
+            var dfactor = Math.pow(10, decimals) || 0;
+            var prefix = cfg.prefix || '';
+            var values = [];
+            var i;
+
+            for (i = min; i < max; i += step) {
+                values.push(prefix + Math.round(dfactor * i) / dfactor);
+            }
+
+            return values;
+        },
+
+        months: function (config) {
+            var cfg = config || {};
+            var count = cfg.count || 12;
+            var section = cfg.section;
+            var values = [];
+            var i, value;
+
+            for (i = 0; i < count; ++i) {
+                value = MONTHS[Math.ceil(i) % 12];
+                values.push(value.substring(0, section));
+            }
+
+            return values;
+        },
+
+        color: function (index) {
+            return COLORS[index % COLORS.length];
+        },
+
+        transparentize: function (color, opacity) {
+            var alpha = opacity === undefined ? 0.5 : 1 - opacity;
+            return Color(color).alpha(alpha).rgbString();
+        }
+    };
+
+    // DEPRECATED
+    window.randomScalingFactor = function () {
+        return Math.round(Samples.utils.rand(-100, 100));
+    };
+
+    // INITIALIZATION
+
+    Samples.utils.srand(Date.now());
+
+    // Google Analytics
+    /* eslint-disable */
+    if (document.location.hostname.match(/^(www\.)?chartjs\.org$/)) {
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date(); a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-28909194-3', 'auto');
+        ga('send', 'pageview');
+    }
+    /* eslint-enable */
+
+}(this));
+
+
+// Bar Chart
+
+var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var color = Chart.helpers.color;
+var barChartData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+        label: 'Dataset 1',
+        backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+        borderColor: window.chartColors.red,
+        borderWidth: 1,
+        data: [
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor()
+        ]
+    }, {
+        label: 'Dataset 2',
+        backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(),
+        borderColor: window.chartColors.blue,
+        borderWidth: 1,
+        data: [
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor()
+        ]
+    }]
+
+};
+
+// Pie Chart
+var randomScalingFactor = function () {
+    return Math.round(Math.random() * 100);
+};
+
+var config = {
+    type: 'pie',
+    data: {
+        datasets: [{
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+            ],
+            backgroundColor: [
+                window.chartColors.red,
+                window.chartColors.orange,
+                window.chartColors.yellow,
+                window.chartColors.green,
+                window.chartColors.blue,
+            ],
+            label: 'Dataset 1'
+        }],
+        labels: [
+            'Red',
+            'Orange',
+            'Yellow',
+            'Green',
+            'Blue'
+        ]
+    },
+    options: {
+        responsive: true
+    }
+};
+
+window.onload = function () {
+    // Bar Chart
+    var barCtx = document.getElementById("barCanvas").getContext("2d");
+    window.myBar = new Chart(barCtx, {
+        type: 'bar',
+        data: barChartData,
+        options: {
+            responsive: true,
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Bar Chart'
+            }
+        }
+    });
+    // Pie Chart
+    var ctx = document.getElementById('chart-area').getContext('2d');
+    window.myPie = new Chart(ctx, config);
+};
+
+
+
